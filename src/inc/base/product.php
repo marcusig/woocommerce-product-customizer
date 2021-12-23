d5dc2e1ec4ae27c6d5d0afe1b44f3ba5149af79c
Marc Lacroix <mlacroix@updraftplus.com>
2021-12-23 21:23:12 +0200

    Better WPML compat

--- src/inc/base/product.php
+++ src/inc/base/product.php
@@ -8,9 +8,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Product {
 	public static function set_layer_item_meta( $layer, $product ) {
+
+		$label = $layer->get_layer( 'name' );
+		$value = $layer->get_choice( 'name' );
+
+		// WPML
+		if ( function_exists( 'wpml_get_current_language' ) && function_exists( 'wpml_get_default_language' ) && wpml_get_default_language() !== wpml_get_current_language() ) {
+			if ( $label_translation = $layer->get_layer( 'name_' . wpml_get_current_language() ) ) {
+				$label = $label_translation;
+			}
+			if ( $value_translation = $layer->get_choice( 'name_' . wpml_get_current_language() ) ) {
+				$value = $value_translation;
+			}
+		}
+		
 		$meta = array(
-			'label' => $layer->get_layer( 'name' ),
-			'value' => $layer->get_choice( 'name' ),
+			'label' => $label,
+			'value' => $value,
 		);
 		return apply_filters( 'mkl_pc_item_meta', (array) $meta, $layer, $product ); 
 	}
