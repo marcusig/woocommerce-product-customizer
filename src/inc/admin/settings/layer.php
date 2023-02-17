40fb0d34ff9b5391f4f5dcc5a2ad00b8fac0d87b
Marc Lacroix <marcus_lacroix@yahoo.fr>
2023-02-17 08:45:03 +0200

    Angle switch on groups

--- src/inc/admin/settings/layer.php
+++ src/inc/admin/settings/layer.php
@@ -150,7 +150,7 @@ if ( ! class_exists('MKL\PC\Layer_Settings') ) {
 				'angle_switch' => array(
 					'label' => __( 'Automatic angle switch', 'product-configurator-for-woocommerce' ),
 					'type' => 'select',
-					'condition' => '!data.not_a_choice && ( "simple" == data.type || "multiple" == data.type )',
+					'condition' => '!data.not_a_choice && ( "simple" == data.type || "multiple" == data.type || "group" == data.type )',
 					'choices' => [
 						[
 							'label' => 'No',
