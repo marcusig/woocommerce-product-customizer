3b4c7c8c357e613461b9c221c3034b232e0dcb79
Marc Lacroix <marcus_lacroix@yahoo.fr>
2023-02-20 10:30:33 +0200

    Group update notices and login

--- src/inc/plugin.php
+++ src/inc/plugin.php
@@ -156,6 +156,159 @@ class Plugin {
 		$this->ajax = new Ajax();
 
 		do_action( 'mkl_pc_is_loaded' );
+
+		add_action( 'core_upgrade_preamble', array($this, 'core_upgrade_preamble'), 10 );
+		add_action('load-plugins.php', array($this, 'load_plugins_php'), 5);
+	}
+
+	public function load_plugins_php() {
+		if (!current_user_can('update_plugins')) return;
+		global $mkl_updater_instances;
+		if ( ! is_array( $mkl_updater_instances ) ) return;
+		foreach( $mkl_updater_instances as $updater_instance ) {
+			remove_action( 'load-plugins.php', [ $updater_instance, 'load_plugins_php' ], 10 );
+		}
+		add_action('all_admin_notices', array($this, 'add_notice'), 5);
+	}
+	public function add_notice() {
+		if (!current_user_can('update_plugins')) return;
+		global $mkl_updater_instances;
+		if ( ! is_array( $mkl_updater_instances ) ) return;
+		?> 
+		<style>
+			.updated.mkl_pc_addons {
+				display: flex;
+				flex-wrap: wrap;
+				border-left-color: #cb3a27;
+				align-items: center;
+			}
+
+			.mkl_pc_addons--description,
+			.mkl_pc_addons--connector{
+				box-sizing: border-box;
+				width: 50%;
+				padding: 20px;
+			}
+
+			.mkl_pc_addons--description {
+				border-right: 1px solid #EEE;
+			}
+
+			.mkl_pc_addons--description img {
+				max-width: 250px;
+			}
+			h3.mkl_pc_addons--warning {
+				padding: 10px;
+				background: #cb3a26;
+				border-radius: 3px;
+				color: #FFF;
+			}
+			.mkl_pc_addons--list {
+				width: 100%;
+				display: none;
+			}
+
+			.mkl_pc_addons--list table tr.active td {
+				border-left: none;
+				border-top: 1px solid #EEE
+			}
+
+			.mkl_pc_addons--list table {
+				width: 100%;
+			}
+
+			[class*="udmupdater_box_woocommerce-mkl-pc"] {
+				display: flex;
+				flex-wrap: wrap;
+			}
+
+			[class*="udmupdater_box_woocommerce-mkl-pc"] div:first-of-type {
+				flex: 2;
+			}
+
+			.udmupdater_userpassform[class*="udmupdater_userpassform_woocommerce-mkl-pc"] {flex: 1;}
+
+			.udmupdater_autoupdate {
+				width: 100%;
+			}
+
+		</style>
+		<div class="updated mkl_pc_addons">
+			<div class="mkl_pc_addons--description">
+				<img src="<?php echo MKL_PC_ASSETS_URL; ?>admin/images/mkl-live-product-configurator-for-woocommerce.png" alt="Product Configurator for WooCommerce"/>
+				<h3 class="mkl_pc_addons--warning">Access to  updates for some add-ons is not active</h3>
+				<p>You haven’t connected some of the add-ons licenses, so you will not receive updates.
+				Keeping the add-ons up to date is recommended. </p>
+				<button class="button button-primary mkl_pc_addons--show-list">Connect now</button>
+			</div>
+			<div class="mkl_pc_addons--list">
+				<div class="mkl_pc_addons--connector">
+					<label for="">Please enter your customer login to access updates:</label><br>
+					<input type="text" style="width:180px;" placeholder="<?php echo esc_attr(__('Email', 'udmupdater')); ?>" name="email" value="">
+					<input type="password" style="width:180px;" placeholder="<?php echo esc_attr(ucfirst(__('password', 'udmupdater'))); ?>" name="password" value="">
+					<button class="button button-primary mkl_pc_addons--connect">Connect all licenses</button>
+					<br><a href="#" class="mkl_pc_addons--show-list">I have several logins, display connections for each add-on.</a>
+				</div>
+				<table>
+				<?php
+				$data = [];
+				foreach( $mkl_updater_instances as $updater_instance ) {
+					if ( $updater_instance && is_callable( [$updater_instance, 'after_plugin_row']) ) {
+						$updater_instance->after_plugin_row( false );
+						$data[] = [
+							'muid' => $updater_instance->muid,
+							'slug' => $updater_instance->slug
+						];
+					}
+				}
+				?>
+				</table>
+			</div>
+		</div>
+		<script>
+			jQuery( function( $ ) {
+				// Show the list 
+				$( '.mkl_pc_addons--show-list' ).on( 'click', function( e ) {
+					e.preventDefault();
+					$( '.mkl_pc_addons--list' ).toggle();
+				} );
+				var plugins_list = <?php echo json_encode( $data ); ?>;
+				// Handle logging in
+				var nonce = '<?php echo esc_js(wp_create_nonce('udmupdater-ajax-nonce')); ?>';
+
+				$('.mkl_pc_addons--connect').click( function() {
+					var button = this;
+					var $box = $(this).closest('.mkl_pc_addons--connector');
+					var email = $box.find('input[name="email"]').val();
+					var password = $box.find('input[name="password"]').val();
+					if (email == '' || password == '') {
+						alert( '<?php echo esc_js( sprintf( __('You need to enter both an email address and a %s', 'udmupdater'), __('password', 'udmupdater') ) ); ?>' );
+						return false;
+					}
+					$.each( plugins_list, function( ind, item ) {
+
+						var sdata = {
+							action: 'udmupdater_ajax',
+							subaction: 'connect',
+							nonce: nonce,
+							userid: item.muid,
+							slug: item.slug,
+							email: email,
+							password: password
+						}
+						$(this).prop('disabled', true).html('<?php echo esc_js(__('Connecting...', 'udmupdater')); ?>');
+						
+						
+					} );
+
+					return false;
+				});				
+
+			} );
+		</script>
+	
+		<?php
+
 	}
 }
 
