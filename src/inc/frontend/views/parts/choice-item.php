882b56c34e899d655bd9407747ad9b3947352e54
Marc Lacroix <marcus_lacroix@yahoo.fr>
2023-02-22 20:59:15 +0200

    toggle_group logic

--- src/inc/frontend/views/parts/choice-item.php
+++ src/inc/frontend/views/parts/choice-item.php
@@ -12,10 +12,23 @@
 
 <script type="text/html" id="tmpl-mkl-pc-configurator-choice-group" data-wg-notranslate>
 	<?php do_action( 'tmpl-pc-configurator-choice-group-item--before' ); ?>
-	<div class="choice-group-label">
-	<?php do_action( 'tmpl-pc-configurator-choice-item' ); ?>
-	<?php do_action( 'tmpl-pc-configurator-choice-item--after' ); ?>
-	</div>
+	<# 
+	
+	var choice_setting = 'undefined' == typeof data.choice_groups_toggle ? 'inherit' : data.choice_groups_toggle;
+	var use_button = ( PC_config.config.choice_groups_toggle && 'inherit' === data.choice_groups_toggle ) || 'enabled' === data.choice_groups_toggle;
+	
+	if ( use_button ) { #>
+		<button class="choice-group-label">
+	<# } else { #>	
+		<div class="choice-group-label">
+	<# } #>
+			<?php do_action( 'tmpl-pc-configurator-choice-item' ); ?>
+			<?php do_action( 'tmpl-pc-configurator-choice-item--after' ); ?>
+	<# if ( use_button ) { #>
+		</button>
+	<# } else { #>	
+		</div>
+	<# } #>
 	<ul class="choices-list--children" data-item-id="{{data._id}}"></ul>
 	<?php do_action( 'tmpl-pc-configurator-choice-group-item--after' ); ?>
 </script>
