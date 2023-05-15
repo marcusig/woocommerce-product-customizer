10b2563d5955ebe4b3529ea7afb395717c9bfd03
Marc Lacroix <marcus_lacroix@yahoo.fr>
2023-05-15 10:16:34 +0200

    Replace image in quote cart

--- src/inc/compatibility/yith-quote-request.php
+++ src/inc/compatibility/yith-quote-request.php
@@ -15,6 +15,7 @@ class Compat_Yith_Raq {
 		add_action( 'yith_raq_updated', [ $this, 'yith_raq_updated' ] );
 		add_filter( 'ywraq_request_quote_view_item_data', [ $this, 'view_item_data' ], 20, 3 );
 		add_filter( 'ywraq_item_data', [ $this, 'item_data' ], 20, 3 );
+		add_filter( 'ywraq_product_image', [ $this, 'item_image' ], 20, 2 );
 		add_filter( 'mkl_pc_js_config', [ $this, 'config' ] );
 		add_action( 'mkl_pc_frontend_configurator_after_add_to_cart', [ $this, 'add_add_to_quote_button' ], 15 );
 	}
@@ -83,6 +84,7 @@ class Compat_Yith_Raq {
 				);
 				$d = apply_filters( 'woocommerce_get_item_data', [], $temp_item_data );
 				$rq->raq_content[ $item_id ][ 'pc_configurator_data' ] = $d;
+				$rq->raq_content[ $item_id ][ 'pc_layers' ] = $layers;
 				if ( ! isset( $rq->raq_content[ $item_id ][ 'variations' ] ) ) $rq->raq_content[ $item_id ][ 'variations' ] = [];
 				$rq->raq_content[ $item_id ][ 'variations' ] = array_merge( $rq->raq_content[ $item_id ][ 'variations' ], $d );
 				$added = true;
@@ -106,6 +108,54 @@ class Compat_Yith_Raq {
 		return $item_data;
 	}
 
+	/**
+	 * Replace the image
+	 *
+	 * @param string $item_image
+	 * @param array $raq
+	 * @return string
+	 */
+	public function item_image( $item_image, $raq ) {
+		if ( ! mkl_pc( 'settings' )->get( 'show_image_in_cart' ) ) return $item_image;
+		if ( isset( $raq['pc_layers'] ) ) {
+			$configurator_data = $raq['pc_layers'];
+			$choices = array(); 
+			usort( $configurator_data, [ $this, '_order_images' ] );
+			foreach ( $configurator_data as $layer ) {
+				if ( ! $layer  || ! is_callable( [ $layer, 'get_image_id' ] ) ) continue;
+				if ( $choice_image = $layer->get_image_id( 'image' ) ) {
+					$choices[] = [ 'image' => $choice_image ];
+				}
+			}
+
+			$configuration = new Configuration( NULL, array( 'product_id' => $raq['product_id'], 'content' => json_encode( $choices ) ) );
+			$size = mkl_pc( 'settings' )->get( 'cart_thumbnail_size', 'woocommerce_thumbnail' );
+			$img = $configuration->get_image( $size );
+
+			if ( $img ) return $img;
+		}		
+		return $item_image;
+	}
+
+	/**
+	 * Order images
+	 *
+	 * @param object $choice_a
+	 * @param object $choice_b
+	 * @return integer
+	 */
+	private function _order_images( $choice_a, $choice_b ) {
+		if ( ! $choice_a || ! $choice_b ) return 0;
+		$a = $choice_a->get_layer( 'image_order' );
+		$b = $choice_b->get_layer( 'image_order' );
+		// fallback to normal sort
+		if ( false === $a ) {
+			$a = $choice_a->get_layer( 'order' );
+			$b = $choice_b->get_layer( 'order' );
+		}
+		return ($a > $b) ? +1 : -1;
+	}
+	
 	/**
 	 * Add the Add to quote button
 	 */
