61e833ee185c923ec41b7e145412b16b75e4cdd8
Marc Lacroix <marcus_lacroix@yahoo.fr>
2023-12-01 16:43:27 +0200

    la pomme compat

--- src/inc/themes/la-pomme/theme.php
+++ src/inc/themes/la-pomme/theme.php
@@ -80,10 +80,8 @@ function mkl_pc_lapomme_theme_filter_colors( $colors ) {
 add_filter( 'mkl_pc_theme_color_settings', 'mkl_pc_lapomme_theme_filter_colors' );
 
 function mkl_pc_lapomme_syd_icon() {
-	return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xml:space="preserve">
-		<path d="m34.9 44.8-9.8-7c-.6-.4-1.5-.4-2.1 0l-9.8 7c-1.2.9-2.9 0-2.9-1.5V4.7c0-1 .8-1.8 1.8-1.8h23.8c1 0 1.8.8 1.8 1.8v38.6c0 1.5-1.6 2.3-2.8 1.5z"/>
-	</svg>
-	';
+	// Icon bookmark-simple from https://phosphoricons.com/
+	return '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M184,32H72A16,16,0,0,0,56,48V224a8,8,0,0,0,12.24,6.78L128,193.43l59.77,37.35A8,8,0,0,0,200,224V48A16,16,0,0,0,184,32Zm0,177.57-51.77-32.35a8,8,0,0,0-8.48,0L72,209.57V48H184Z"></path></svg>';
 }
 add_filter( 'PC.syd.svg.icon', 'mkl_pc_lapomme_syd_icon' );
 
@@ -91,3 +89,19 @@ function mkl_pc_clean_add_reset_icon() {
 	echo file_get_contents( trailingslashit( plugin_dir_path( __FILE__ ) ) . 'images/reset.svg' );
 }
 add_action( 'mkl_pc/reset_button/before_label', 'mkl_pc_clean_add_reset_icon' );
+
+function mkl_pc_lapomme_save_pdf_icon() {
+	echo file_get_contents( trailingslashit( plugin_dir_path( __FILE__ ) ) . 'images/download.svg' );
+}
+add_action( 'PC.syd.pdf_download.svg.icon', 'mkl_pc_lapomme_save_pdf_icon' );
+
+function mkl_pc_lapomme_previous_step_icon() {
+	echo file_get_contents( trailingslashit( plugin_dir_path( __FILE__ ) ) . 'images/arrow-left.svg' );
+}
+add_action( 'mkl_pc/previous_step/before', 'mkl_pc_lapomme_previous_step_icon' );
+
+function mkl_pc_lapomme_next_step_icon() {
+	echo file_get_contents( trailingslashit( plugin_dir_path( __FILE__ ) ) . 'images/arrow-right.svg' );
+}
+add_action( 'mkl_pc/next_step/after', 'mkl_pc_lapomme_next_step_icon' );
+
