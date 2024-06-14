da81e11f33d833c479c93f2400429c33331a0180
Marc Lacroix <marcus_lacroix@yahoo.fr>
2024-06-14 08:00:05 +0200

    Update product_configurator.js

--- src/assets/js/product_configurator.js
+++ src/assets/js/product_configurator.js
@@ -298,7 +298,7 @@ PC.actionParameter = 'pc_get_data';
 		PC.fe.active_product = product_id; 
 		PC.fe.parent_product = parent_id ? parent_id : product_id;
 		
-		if ( PC.productData['prod_'+parent_id] ) {
+		if ( PC.productData && PC.productData['prod_'+parent_id] ) {
 			this.modal = this.modal || new PC.fe.views.configurator( { product_id: product_id, parent_id: parent_id } ); 
 			PC.fe.init( product_id, parent_id, $element );
 		} else if ( PC.productDataMode && "json" == PC.productDataMode ) {
