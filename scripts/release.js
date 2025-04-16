438f297b53730acf5cb2593fb06a2d6b78b5f201
Marc Lacroix <marcus_lacroix@yahoo.fr>
2025-04-16 11:59:38 +0200

    build tool

---
+++ scripts/release.js
@@ -0,0 +1,35 @@
+const { execSync } = require('child_process');
+const fs = require('fs-extra');
+const path = require('path');
+
+const args = require('minimist')(process.argv.slice(2));
+const version = args.v;
+console.log( version );
+if (!version) {
+  console.error('❌ Please provide a version with -v');
+  process.exit(1);
+}
+
+// Paths
+const distDir = path.resolve(__dirname, '../src/plugins/woocommerce-mkl-product-customizer/dist');
+const svnTrunk = path.resolve(__dirname, '../repository/product-configurator-for-woocommerce/trunk');
+const repoUrl = 'http://plugins.svn.wordpress.org/product-configurator-for-woocommerce';
+
+console.log(`🚚 Copying dist → trunk`);
+fs.emptyDirSync(svnTrunk);
+fs.copySync(distDir, svnTrunk);
+
+// Detect new/deleted files
+console.log('🔍 Running svn add/delete');
+execSync(`svn add --force "${svnTrunk}" --auto-props --parents --depth infinity`, { stdio: 'inherit' });
+execSync(`svn status "${svnTrunk}" | grep '^!' | awk '{print $2}' | xargs -r svn delete`, { shell: '/bin/bash', stdio: 'inherit' });
+
+// Commit
+console.log(`✅ Committing version ${version}`);
+execSync(`svn commit "${svnTrunk}" -m "v${version}"`, { stdio: 'inherit' });
+
+// Tag
+console.log(`🏷️  Tagging version ${version}`);
+execSync(`svn copy ${repoUrl}/trunk ${repoUrl}/tags/${version} -m "Tagging version ${version}"`, { stdio: 'inherit' });
+
+console.log(`🎉 Done! Plugin tagged as ${version}`);
