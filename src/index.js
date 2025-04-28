263df6849285c07bd231fc99c2cdb619baad095f
Marc Lacroix <marcus_lacroix@yahoo.fr>
2025-04-28 11:13:28 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './blocks/configurator-form';
+import './blocks/configurator-actions';
+import './blocks/configurator-toolbar';
+import './blocks/configurator-viewer';
+
+import './blocks/configurator';
