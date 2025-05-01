571944623a98babc7e38de41aa7f3af2efafbe67
Marc Lacroix <marcus_lacroix@yahoo.fr>
2025-05-01 15:10:09 +0200

    Fix release script

--- scripts/release.js
+++ scripts/release.js
@@ -11,10 +11,13 @@ if (!version) {
 }
 
 // Paths
-const distDir = path.resolve(__dirname, '../src/plugins/woocommerce-mkl-product-customizer/dist');
-const svnTrunk = path.resolve(__dirname, '../repository/product-configurator-for-woocommerce/trunk');
+const distDira = path.resolve(__dirname);
+const distDir = path.resolve(__dirname, '../dist');
+const svnTrunk = path.resolve(__dirname, '../../../../repository/product-configurator-for-woocommerce/trunk');
 const repoUrl = 'http://plugins.svn.wordpress.org/product-configurator-for-woocommerce';
 
+console.log( distDira );
+console.log( svnTrunk );
 console.log(`🚚 Copying dist → trunk`);
 fs.emptyDirSync(svnTrunk);
 fs.copySync(distDir, svnTrunk);
@@ -23,6 +26,10 @@ fs.copySync(distDir, svnTrunk);
 console.log('🔍 Running svn add/delete');
 execSync(`svn add --force "${svnTrunk}" --auto-props --parents --depth infinity`, { stdio: 'inherit' });
 execSync(`svn status "${svnTrunk}" | grep '^!' | awk '{print $2}' | xargs -r svn delete`, { shell: '/bin/bash', stdio: 'inherit' });
+
+// Dry run
+// console.log(`svn commit "${svnTrunk}" -m "v${version}"`);
+// console.log(`svn copy ${repoUrl}/trunk ${repoUrl}/tags/${version} -m "Tagging version ${version}"`);
 
 // Commit
 console.log(`✅ Committing version ${version}`);
