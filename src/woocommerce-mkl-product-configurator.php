f3eb71e689aecf4c0bfdf094979dc4578ef29660
Marc Lacroix <marcus_lacroix@yahoo.fr>
2025-07-15 10:24:28 +0200

    Update woocommerce-mkl-product-configurator.php

--- src/woocommerce-mkl-product-configurator.php
+++ src/woocommerce-mkl-product-configurator.php
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-define( 'MKL_PC_VERSION', '1.4.4' );
+define( 'MKL_PC_VERSION', '1.5.0' );
 define( 'MKL_PC_PREFIX', '_mkl_pc_' );
 define( 'MKL_PC_EXTENDS', 'woocommerce' ); 
 define( 'MKL_PC_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
