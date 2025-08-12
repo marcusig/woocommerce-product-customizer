0a3e798a1504649b6ba586e25712169adb4f7e7b
Marc <marcusig@users.noreply.github.com>
2025-08-12 09:48:55 +0200

    add JS filter 'PC.fe.tooltip.add_all_text'

--- src/assets/js/views/parts/choice.js
+++ src/assets/js/views/parts/choice.js
@@ -91,7 +91,7 @@ PC.fe.views.choice = Backbone.View.extend({
 		}
 	},
 	get_description: function() {
-		if ( 'colors' == this.model.collection.layer.get( 'display_mode' ) && ! this.model.get( 'is_group' ) ) {
+		if ( wp.hooks.applyFilters( 'PC.fe.tooltip.add_all_text', 'colors' == this.model.collection.layer.get( 'display_mode' ) && ! this.model.get( 'is_group' ) ) ) {
 			this.update_tippy_on_price_update = true;
 			var description = this.$( '.choice-text' ).length ? this.$( '.choice-text' ).html() : this.$( '.choice-name' ).html();
 			if ( this.$( '.choice-price' ).length ) {
