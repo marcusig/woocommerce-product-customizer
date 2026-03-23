d3a31129e754b13e1290f58546903a43d79f0316
Marc <marcusig@users.noreply.github.com>
2026-03-23 13:08:12 +0200

    wsb np-toggle support

--- src/inc/themes/wsb/wsb.js
+++ src/inc/themes/wsb/wsb.js
@@ -26,6 +26,8 @@
 	wp.hooks.addAction( 'PC.fe.layer.activate', 'MKL/PC/Themes/wsb', function( view ) {
 		if ( ! wp.hooks.applyFilters( 'pc.themes.wsb.toggle_choices', true, view ) ) return;
 		if ( PC_config.config.no_toggle && 'dropdown' != view.model.get( 'display_mode' ) ) return;
+		if ( view.model.get( 'class_name' ) && view.model.get( 'class_name' ).includes( 'no-toggle' ) ) return;
+
 		view.$el.find( '.layer_choices' ).first().delay(40).slideDown( { step: function() {
 			if ( PC_config.config.auto_scroll && view.el.offsetParent ) view.el.offsetParent.scrollTo( 0, view.el.offsetTop );
 		} } );
@@ -37,6 +39,7 @@
 	wp.hooks.addAction( 'PC.fe.layer.deactivate', 'MKL/PC/Themes/wsb', function( view ) {
 		if ( ! wp.hooks.applyFilters( 'pc.themes.wsb.toggle_choices', true, view ) ) return;
 		if ( PC_config.config.no_toggle && 'dropdown' != view.model.get( 'display_mode' ) ) return;
+		if ( view.model.get( 'class_name' ) && view.model.get( 'class_name' ).includes( 'no-toggle' ) ) return;
 		view.$el.find( '.layer_choices' ).first().slideUp(200);
 	} );
 
