ab7a69770d857ab28a8cd21d0827bd97181ebcd8
Marc <marcusig@users.noreply.github.com>
2026-04-29 16:16:13 +0200

    Include menu, improve loading

--- src/assets/admin/js/views/app.js
+++ src/assets/admin/js/views/app.js
@@ -117,76 +117,172 @@ PC.views = PC.views || {};
 		className: 'pc-modal-container',
 		template : wp.media.template( 'mkl-modal' ),
 		loading: 0,
-		initialize: function( options ){ 
+		initialize: function( options ) { 
 
 			this.product = options.current_product;
 			this.states = new PC.states();
 			this.admin = PC.app.get_admin();
+			this.loading = 0;
+
+			var menuInlined = PC.states.hasInlinedMenu();
+
 			if ( ! this.admin.structure ) {
-				this.loading ++;
-
-				PC.app.admin_data.fetch({
+				this.loading++;
+				PC.app.admin_data.fetch( {
 					data: this.product.id != PC.app.id ? { variation_id: this.product.id } : {},
-					success: _.bind(function( model, res, options ) {
-						const configurator_source = PC.app.admin_data.get( 'configurator_source' ) || 'local';
-						this.$el.addClass( 'mkl-pc-configurator-is-' + configurator_source );
-
-						this.admin.set_data();
-
-						this.fetched( model, res, options );
-
-						if ( this.contentMissing ) {
-							this.contentMissing = false;
-							this.product.fetch({
-								success: _.bind(this.fetched, this),
-								error: function(model, res, options) {
-									console.log('error fecthing data');
-									console.log( model, res, options );
-								}
-							});
-						}
-
-		
-					}, this),
-					error: function(model, res, options) {
-						console.log('error fecthing data');
-						console.log( model, res, options );
-					}
-				}); 
-
+					success: _.bind( this.onAdminDataLoaded, this ),
+					error: _.bind( this.onAdminDataLoadError, this )
+				} );
 			}
 
 			if ( ! this.product.get( 'content' ) ) {
-				this.loading ++;
+				this.loading++;
 				this.contentMissing = true;
+			} else {
+				this.contentMissing = false;
 			}
 
 			this.open();
 
-			this.$el.addClass('loading'); 
-			// fetch the states from the server
-			this.states.fetch( {
-				// when received, executes this.fetched
-				url: this.states.url() + '&id=' + this.product.id, 
-				success: _.bind(this.fetched, this)
+			this.$el.addClass( 'loading' );
 
-			} );
-			this.loading ++;
+			if ( menuInlined && typeof PC_lang !== 'undefined' && PC_lang.admin_menu ) {
+				this.states.reset( PC_lang.admin_menu, { parse: true } );
+			} else {
+				this.loading++;
+				this.states.fetch( {
+					url: this.states.url() + '&id=' + this.product.id,
+					success: _.bind( this.fetched, this ),
+					error: _.bind( this.onMenuLoadError, this )
+				} );
+			}
+
+			if ( this.loading === 0 ) {
+				this.refresh();
+				this.$el.removeClass( 'loading' );
+				this.clearLoadErrorUI();
+			}
 			return this;
+		},
 
+		onAdminDataLoaded: function( model, res, options ) {
+			var configurator_source = PC.app.admin_data.get( 'configurator_source' ) || 'local';
+			this.$el.addClass( 'mkl-pc-configurator-is-' + configurator_source );
+
+			this.admin.set_data();
+
+			this.fetched( model, res, options );
+
+			if ( this.contentMissing ) {
+				this.contentMissing = false;
+				this.product.fetch( {
+					success: _.bind( this.fetched, this ),
+					error: _.bind( this.onProductContentLoadError, this )
+				} );
+			}
+		},
+
+		onAdminDataLoadError: function( model, res, options ) {
+			if ( window.console && console.log ) {
+				console.log( 'error fetching configurator init data', model, res, options );
+			}
+			this.handleLoadError( 'init', res );
+		},
+
+		onMenuLoadError: function( collection, res, options ) {
+			if ( window.console && console.log ) {
+				console.log( 'error fetching configurator menu', collection, res, options );
+			}
+			this.handleLoadError( 'menu', res );
+		},
+
+		onProductContentLoadError: function( model, res, options ) {
+			if ( window.console && console.log ) {
+				console.log( 'error fetching product content', model, res, options );
+			}
+			this.handleLoadError( 'content', res );
+		},
+
+		handleLoadError: function( step, xhr ) {
+			this.loading = 0;
+			this.$el.removeClass( 'loading' );
+			this.$el.addClass( 'load-error' );
+			var msg = ( typeof PC_lang !== 'undefined' && PC_lang.editor_load_failed )
+				? PC_lang.editor_load_failed
+				: 'Could not load the configurator.';
+			var $screen = this.$el.find( '.loading-screen' );
+			$screen.find( '.mkl-pc-editor-load--busy' ).attr( 'hidden', true ).hide();
+			var $err = $screen.find( '.mkl-pc-editor-load--error' );
+			$err.removeAttr( 'hidden' ).show();
+			$err.find( '.mkl-pc-editor-load__message' ).text( msg );
+			if ( window.wp && wp.a11y && wp.a11y.speak ) {
+				wp.a11y.speak( msg );
+			}
+		},
+
+		clearLoadErrorUI: function() {
+			this.$el.removeClass( 'load-error' );
+			var $screen = this.$el.find( '.loading-screen' );
+			$screen.find( '.mkl-pc-editor-load--error' ).attr( 'hidden', true ).hide();
+			$screen.find( '.mkl-pc-editor-load--busy' ).removeAttr( 'hidden' ).show();
+		},
+
+		retryLoad: function( e ) {
+			if ( e && e.preventDefault ) {
+				e.preventDefault();
+			}
+			this.clearLoadErrorUI();
+			this.$el.addClass( 'loading' );
+
+			var menuInlined = PC.states.hasInlinedMenu();
+			this.loading = 0;
+
+			if ( ! this.admin.structure ) {
+				this.loading++;
+				PC.app.admin_data.fetch( {
+					data: this.product.id != PC.app.id ? { variation_id: this.product.id } : {},
+					success: _.bind( this.onAdminDataLoaded, this ),
+					error: _.bind( this.onAdminDataLoadError, this )
+				} );
+			}
+
+			if ( ! this.product.get( 'content' ) ) {
+				this.loading++;
+				this.contentMissing = true;
+			} else {
+				this.contentMissing = false;
+			}
+
+			if ( menuInlined && typeof PC_lang !== 'undefined' && PC_lang.admin_menu ) {
+				this.states.reset( PC_lang.admin_menu, { parse: true } );
+			} else {
+				this.loading++;
+				this.states.fetch( {
+					url: this.states.url() + '&id=' + this.product.id,
+					success: _.bind( this.fetched, this ),
+					error: _.bind( this.onMenuLoadError, this )
+				} );
+			}
+
+			if ( this.loading === 0 ) {
+				this.refresh();
+				this.$el.removeClass( 'loading' );
+				this.clearLoadErrorUI();
+			}
 		},
  		// States are fecthed
 		fetched: function( model, response, options ) {
-			this.loading --;
+			this.loading--;
 			if( this.loading == 0 ) {
 				this.refresh(); 
-				this.$el.removeClass('loading');
+				this.$el.removeClass( 'loading' );
+				this.clearLoadErrorUI();
 			}
 		},
 
 		events: {
 			'click .mkl-pc-admin-ui__close': 'close',
-			// 'click .pc-modal-backdrop': 'close',
+			'click .mkl-pc-editor-load__retry': 'retryLoad',
 		},
 
 		open: function() {
@@ -213,8 +309,15 @@ PC.views = PC.views || {};
 		},
 
 		close: function() {
-			if( _.indexOf( _.values( PC.app.is_modified ), true ) != -1 ) { 
-				if( !confirm( PC.lang.confirm_closing || 'Some values have not been saved. Are you sure you want to close?' ) ) 
+			if ( this.$el.hasClass( 'load-error' ) ) {
+				var closeMsg = ( typeof PC_lang !== 'undefined' && PC_lang.editor_close_after_load_error )
+					? PC_lang.editor_close_after_load_error
+					: 'The configurator did not finish loading. Close anyway?';
+				if ( ! window.confirm( closeMsg ) ) {
+					return false;
+				}
+			} else if ( _.indexOf( _.values( PC.app.is_modified ), true ) != -1 ) {
+				if( !confirm( PC.lang.confirm_closing || 'Some values have not been saved. Are you sure you want to close?' ) )
 					return false;
 			}
 			this.opened = false;
